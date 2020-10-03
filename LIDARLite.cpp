5067e9a8050bf925e6e084cca2083ca061ce3e54
Anagha M Anil Kumar <43451020+Anagha18@users.noreply.github.com>
2020-10-03 16:12:07 +0530

    Update LIDARLite.cpp

--- LIDARLite.cpp
+++ LIDARLite.cpp
@@ -1,53 +1,14 @@
-/*------------------------------------------------------------------------------
 
-  LIDARLite Arduino Library
-  LIDARLite.cpp
-
-  This library provides quick access to the basic functions of LIDAR-Lite
-  via the Arduino interface. Additionally, it can provide a user of any
-  platform with a template for their own application code.
-
-  Copyright (c) 2016 Garmin Ltd. or its subsidiaries.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-------------------------------------------------------------------------------*/
 
 #include <Arduino.h>
 #include <Wire.h>
 #include <stdarg.h>
 #include "LIDARLite.h"
 
-/*------------------------------------------------------------------------------
-  Constructor
 
-  Use LIDARLite::begin to initialize.
-------------------------------------------------------------------------------*/
 LIDARLite::LIDARLite(){}
 
-/*------------------------------------------------------------------------------
-  Begin
 
-  Starts the sensor and I2C.
-
-  Parameters
-  ------------------------------------------------------------------------------
-  configuration: Default 0. Selects one of several preset configurations.
-  fasti2c: Default 100 kHz. I2C base frequency.
-    If true I2C frequency is set to 400kHz.
-  lidarliteAddress: Default 0x62. Fill in new address here if changed. See
-    operating manual for instructions.
-------------------------------------------------------------------------------*/
 void LIDARLite::begin(int configuration, bool fasti2c, char lidarliteAddress)
 {
   Wire.begin(); // Start I2C
